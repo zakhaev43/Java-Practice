32b41f2063d00aea02bab48cf1bcef2ca73a2ceb
imransuet <imransuet@gmail.com>
2020-11-13 17:20:03 +0100

    Builder Pattern

--- src/DPAF.java
+++ src/DPAF.java
@@ -1,13 +1,59 @@
-import strategyPattern.*;
+import builderPattern.*;
 public class DPAF {
-	 public static void main(String[] args) {
-	      Context context = new Context(new OperationAdd());		
-	      System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+	
+	/*
+	 
+	 Problems without builder pattern 
+	 
+	1) too many constructors to maintain.
+    2) You can partially solve this problem by creating Pizza and then adding
+    ingredients but that will impose another problem of leaving Object on
+    inconsistent state during building, ideally cake should not be available until its created.
+    Both of these problem can be solved by using Builder design pattern in Java. 
+    Builder design pattern not only improves readability but also reduces chance of error by adding ingredients 
+    explicitly and making object available ONCE FULLY CONSTRUCTED. 
+    
+	 */
 
-	      context = new Context(new OperationSubstract());		
-	      System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
-
-	      context = new Context(new OperationMultiply());		
-	      System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
-	   }
+	public static void main(String[] args)
+	{
+		PizzaBuilder builder=new ClassicPizzaBuilder();
+		
+		Pizza p1=new Pizza("Rohit Singh");
+		Pizza p2=new Pizza("Alok Pandey");
+		
+		builder.provideOrderName(p1.getName());
+		
+		p1=builder.addCheese()
+				  .addMushroom()
+				  .getPizza();
+		
+		builder.provideOrderName(p2.getName());
+		
+		p2=builder.addCheese()
+				  .addMushroom()
+				  .addPineApple()
+				  .addTomato()
+				  .getPizza();
+		
+		
+		getDescription(p1);
+		
+		System.out.println();
+		
+		getDescription(p2);
+		       
+	}
+	
+	
+	public static void getDescription(Pizza pizza)
+	{
+		System.out.println("Pizza description");
+		System.out.println("Pizza Order name : "+pizza.getName());
+		System.out.println("Cheese : "+pizza.hasCheese());
+		System.out.println("Tomato : "+pizza.hasTomato());
+		System.out.println("Mushroom : "+pizza.hasMushroom());
+		System.out.println("PineApple : "+pizza.hasPineApple());
+	}
+	
 }
---
+++ src/builderPattern/PizzaBuilder.java
@@ -0,0 +1,16 @@
+package builderPattern;
+
+public interface PizzaBuilder {
+	
+	public void provideOrderName(String orderName);
+	
+	public PizzaBuilder addCheese();
+	
+	public PizzaBuilder addTomato();
+
+	public PizzaBuilder addMushroom();
+	
+	public PizzaBuilder addPineApple();
+	
+	public Pizza getPizza();
+}
