06adb70e84de3cfb030b7ea926efb7688df96209
imransuet <imransuet@gmail.com>
2020-11-15 21:29:50 +0100

    Prototype Pattern

--- src/DPAF.java
+++ src/DPAF.java
@@ -1,59 +1,18 @@
-import builderPattern.*;
+import protoTypePattern.*;
 public class DPAF {
 	
-	/*
-	 
-	 Problems without builder pattern 
-	 
-	1) too many constructors to maintain.
-    2) You can partially solve this problem by creating Pizza and then adding
-    ingredients but that will impose another problem of leaving Object on
-    inconsistent state during building, ideally cake should not be available until its created.
-    Both of these problem can be solved by using Builder design pattern in Java. 
-    Builder design pattern not only improves readability but also reduces chance of error by adding ingredients 
-    explicitly and making object available ONCE FULLY CONSTRUCTED. 
-    
-	 */
+	
+	
+	public static void main(String[] args) {
+	      ShapeCache.loadCache();
 
-	public static void main(String[] args)
-	{
-		PizzaBuilder builder=new ClassicPizzaBuilder();
-		
-		Pizza p1=new Pizza("Rohit Singh");
-		Pizza p2=new Pizza("Alok Pandey");
-		
-		builder.provideOrderName(p1.getName());
-		
-		p1=builder.addCheese()
-				  .addMushroom()
-				  .getPizza();
-		
-		builder.provideOrderName(p2.getName());
-		
-		p2=builder.addCheese()
-				  .addMushroom()
-				  .addPineApple()
-				  .addTomato()
-				  .getPizza();
-		
-		
-		getDescription(p1);
-		
-		System.out.println();
-		
-		getDescription(p2);
-		       
-	}
-	
-	
-	public static void getDescription(Pizza pizza)
-	{
-		System.out.println("Pizza description");
-		System.out.println("Pizza Order name : "+pizza.getName());
-		System.out.println("Cheese : "+pizza.hasCheese());
-		System.out.println("Tomato : "+pizza.hasTomato());
-		System.out.println("Mushroom : "+pizza.hasMushroom());
-		System.out.println("PineApple : "+pizza.hasPineApple());
-	}
-	
+	      Shape clonedShape = (Shape) ShapeCache.getShape("1");
+	      System.out.println("Shape : " + clonedShape.getType());		
+
+	      Shape clonedShape2 = (Shape) ShapeCache.getShape("2");
+	      System.out.println("Shape : " + clonedShape2.getType());		
+
+	      Shape clonedShape3 = (Shape) ShapeCache.getShape("3");
+	      System.out.println("Shape : " + clonedShape3.getType());		
+	   }
 }
---
+++ src/protoTypePattern/Square.java
@@ -0,0 +1,14 @@
+package protoTypePattern;
+
+public class Square extends Shape {
+	
+	public Square(){
+	     type = "Square";
+	   }
+
+	   @Override
+	   public void draw() {
+	      System.out.println("Inside Square::draw() method.");
+	   }
+
+}
